d6f1739cd986b826bba8c9948ed5918d782961c4
jorker <jokerjuster@gmail.com>
2021-08-09 23:54:13 +0700

    End of Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,5 +4,7 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] PasswrodHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
